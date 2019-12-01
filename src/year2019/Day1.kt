b7d0097973b11564a363d7c41eb249c19632e0d4
Ed <edward.dobson3@ntlworld.com>
2019-12-01 13:03:40 +0000

    Day1

--- src/year2019/Day1.kt
+++ src/year2019/Day1.kt
@@ -1,18 +1,37 @@
 package year2019
 
 import year2019.util.getContents
+import year2019.util.getLines
 
-fun main() {
+fun main(args: Array<String>) {
     Day1.a()
+    Day1.b()
 }
 
 private object Day1 {
     fun a() {
-        print(getContents("day1a.txt"))
-    }
+        println(getLines("day1a.txt")
+                .map { it.toInt() }
+                .map { it / 3 }
+                .map { Math.floor(it.toDouble()) }
+                .map { it - 2 }
+                .sum())
+}
 
     fun b() {
-        
+        println(getLines("day1a.txt")
+                .map { it.toInt() }
+                .map {
+                    var tot = 0
+                    var massLeft = it
+                    while (true) {
+                        massLeft = (massLeft / 3) - 2
+                        if (massLeft < 0) break
+                        tot += massLeft
+                    }
+                    tot
+                }
+                .sum())
     }
 
 }
--- src/year2019/util/input.kt
+++ src/year2019/util/input.kt
@@ -3,3 +3,5 @@ package year2019.util
 import java.io.File
 
 fun getContents(fileName: String): String = File("./input/$fileName").readText(Charsets.UTF_8)
+
+fun getLines(fileName: String): List<String> = File("./input/$fileName").readLines(Charsets.UTF_8)
